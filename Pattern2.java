cc607f8e404a0de61e112453eaa948518889da33
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2026-01-26 21:01:49 +0530

    Pattern 2 Program Added

---
+++ Pattern2.java
@@ -0,0 +1,20 @@
+class Pattern2
+{
+	public static void main(String[] args) 
+	{
+		int n = 6;
+		for (int i=1; i<n ;i++ )
+		{
+			for (int j = (n-i); j>=1 ; j--)
+			{
+				System.out.print(j+"*");
+			}
+			for (int j = (n-i); j>=1 ;j-- ) 
+			{
+				System.out.print(j+"*");
+			}
+			System.out.println();
+		}
+
+	}
+}
